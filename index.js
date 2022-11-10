def122956ee943176c111c6bffec468b73405ef0
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-11-10 10:18:28 +0600

    set item in locale storage

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("object");
+const addProduct = () => {
+  const product = document.getElementById("product").value;
+  const quantity = document.getElementById("quantity").value;
+  console.log(product, quantity);
+  const cart = getStoredCart();
+  cart[product] = quantity;
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+};
+
+const getStoredCart = () => {
+  const storedCart = localStorage.getItem("cart");
+  let cart = {};
+  if (storedCart) {
+    cart = JSON.parse(storedCart);
+  }
+  return cart;
+};
